fabc6abba4b9f0c85088733f71ad3bd91f2ed974
kasina0611 <saiganesh01.k@gmail.com>
2021-05-18 14:49:22 +0530

    npm scripts-1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval:2000});
+    $('#carouselbutton').click(function(){
+        if ($('#carouselbutton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselbutton').children('span').removeClass('fa-pause');
+            $('#carouselbutton').children('span').addClass('fa-play');
+        }
+        else{
+            $('#mycarousel').carousel('cycle');
+            $('#carouselbutton').children('span').removeClass('fa-play');
+            $('#carouselbutton').children('span').addClass('fa-pause');
+        }
+    });
+    $('#loginbutton').click(function(){
+        $('#loginmodal').modal('show');
+    });
+    $('#reservebutton').click(function(){
+        $('#reservemodal').modal('show');
+    });
+});
